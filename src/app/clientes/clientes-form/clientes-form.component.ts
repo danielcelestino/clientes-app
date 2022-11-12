fbe51cc23dbb5d72db88b3d87c499637289e7e6d
Daniel Celestino <danielcelestino@gmail.com>
2022-11-12 19:06:36 -0300

    Refactor depreceated method subscribe and refactor item ID and dataCadastro

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Cliente } from '../cliente';
 import { ClientesService } from '../../clientes.service';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-clientes-form',
@@ -22,16 +23,66 @@ export class ClientesFormComponent implements OnInit {
   }
 
   onSubmit(){
-    this.service
-      .salvar(this.cliente)
-      .subscribe(response => {
-          console.log(response);
-          this.success=true;
-        }, errorResponse => {
-            this.erros = errorResponse.error.listErrors;
-            console.log(errorResponse.error.listErrors);
-        }
-      );
+    this.service.salvar(this.cliente).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.success=true;
+        this.erros = [];
+        this.cliente = data;
+        console.log(data);        
+      },
+      error: (error) => {
+          this.success=false;
+          this.erros = error.error.listErrors;  
+           console.log(error);        
+      }
+    }); 
+       
+          //this.service
+   //   .salvar(this.cliente)
+    //  .subscribe(response => {
+    //      console.log(response);
+    //      this.success=true;
+     //     this.erros = [];
+    //      console.log(response);
+    //    }, errorResponse => {
+    //      this.success=false;
+    //        this.erros = errorResponse.error.listErrors;            
+    //    }
+    //  );
+
+
+
+
+
+
+       // this.userService.addUser(this.user).subscribe(
+    //  (data)=>{
+        //success
+    //    console.log(data);
+    //    this._snack.open('Registered Successfully', 'OK', {
+   //       duration: 2000,
+   //       verticalPosition: 'top',
+    //      horizontalPosition: 'center',
+    //      panelClass: ['green-snackbar', 'login-snackbar'],
+   //     });
+   ///   },
+   //   (error)=>{
+    //    //error
+   //     console.log(error);
+   //     this._snack.open('Something went Wrong', 'OK', {
+   //       duration: 2000,
+   //       verticalPosition: 'top',
+   //       horizontalPosition: 'center',
+   //       panelClass: ['red-snackbar','login-snackbar'],
+   //     });
+   //   }
+  //  );
+
+
+
+
+    
   }
 
 }

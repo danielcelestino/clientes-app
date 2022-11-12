eea225c7baf63b7b00c612927ecfb12efedbf6ba
Daniel Celestino <danielcelestino@gmail.com>
2022-11-12 17:08:34 -0300

    Connect with back end to save new cliente - post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { HomeComponent } from './home/home.component'
 import { ClientesFormComponent } from './clientes/clientes-form/clientes-form.component';
 import { ClientesModule } from './clientes/clientes.module';
 
+import { ClientesService } from './clientes.service'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +19,14 @@ import { ClientesModule } from './clientes/clientes.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule
   ],
-  providers: [],
+  providers: [
+    ClientesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Cliente } from '../cliente';
+import { ClientesService } from '../../clientes.service';
 
 @Component({
   selector: 'app-clientes-form',
@@ -11,16 +12,17 @@ export class ClientesFormComponent implements OnInit {
 
   cliente?: Cliente;
 
-  constructor() { 
-    this.cliente = new Cliente();
-    this.cliente.nome = 'Daniel';
+  constructor(private service: ClientesService) { 
+    this.cliente = service.getCliente();
   }
 
   ngOnInit(): void {
   }
 
-  salvar(){
-    console.log('cliquei!');
+  onSubmit(){
+    this.service.salvar(this.cliente).subscribe(response => {
+      console.log(response);
+    });
   }
 
 }

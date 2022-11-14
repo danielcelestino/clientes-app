ddbb9a49088f5410ed8e5385fcca051b7b3d7b56
Daniel Celestino <danielcelestino@gmail.com>
2022-11-14 10:05:16 -0300

    Method post of Srviços Prestados with cors configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,9 @@ import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component'
 import { ClientesFormComponent } from './clientes/clientes-form/clientes-form.component';
 import { ClientesModule } from './clientes/clientes.module';
-
-import { ClientesService } from './clientes.service'
+import { ClientesService } from './clientes.service';
+import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
+import { ServicoPrestadoService } from './servico-prestado.service';
 
 @NgModule({
   declarations: [
@@ -22,10 +23,12 @@ import { ClientesService } from './clientes.service'
     HttpClientModule,
     AppRoutingModule,
     TemplateModule,
-    ClientesModule
+    ClientesModule,
+    ServicoPrestadoModule    
   ],
   providers: [
-    ClientesService
+    ClientesService,
+    ServicoPrestadoService 
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../clientes/cliente';
+import { ClientesService } from '../../clientes.service';
+import { ServicoPrestado } from '../servicoPrestado';
+import { ServicoPrestadoService } from '../../servico-prestado.service'
+
+@Component({
+  selector: 'app-servico-prestado-form',
+  templateUrl: './servico-prestado-form.component.html',
+  styleUrls: ['./servico-prestado-form.component.css']
+})
+export class ServicoPrestadoFormComponent implements OnInit {
+
+  clientes: Cliente[] = [];
+  servico : ServicoPrestado;
+  success: boolean = false;
+  erros?: string[];
+
+  constructor(
+    private clienteService: ClientesService,
+    private servicoPrestadoService: ServicoPrestadoService
+  ) { 
+    this.servico = new ServicoPrestado();
+  }
+
+  ngOnInit(): void {
+    this.clienteService.getClientes().subscribe( response => this.clientes = response);
+  }
+
+  onSubmit(){
+    console.log(this.servico);
+    this.servicoPrestadoService.salvar(this.servico).subscribe({
+      next: (data) => {        
+        this.success=true;
+        this.erros = [];
+        this.servico = data;     
+      },
+      error: (error) => {
+          this.success=false;
+          this.erros = error.error.listErrors;        
+      }
+    }); 
+
+  }
+
+}
